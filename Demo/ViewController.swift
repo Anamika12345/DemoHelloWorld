36238d5f4dda1d172d884c54b8ed837ca708972d
anamika.sinha@mashvirtual.in <anamika.sinha@mashvirtual.in>
2019-01-18 12:36:18 +0530

    first commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Mash Virtual on 17/01/19.
+//  Copyright © 2019 Mash Virtual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Anamika Sinha")
+    }
+
+
+}
+
